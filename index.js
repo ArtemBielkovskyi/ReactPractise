b75d774fc450e759e24cf43a396a0832b7053ed2
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-03-12 09:31:10 +0000

    First part of the course project

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-function Content() {
-    return (
-        <div>
-            <h1>Content</h1>
-            <p>The content text!!!</p>
-        </div>
-    )
-}
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <Content />,
-    document.getElementById('root')
-);
+// function Content() {
+//     return (
+//         <div>
+//             <h1>Content</h1>
+//             <p>The content text!!!</p>
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(
+//     <Content />,
+//     document.getElementById('root')
+// );
 
 // const h1 = document.createElement('h1');
 // h1.textContent = 'Content';
 // h1.className = 'content';
 // document.getElementById("root2").appendChild(h1);
 
-ReactDOM.render(<div className="text">Some text practise</div>, document.getElementById('root2'));
+
+const navBar = (
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/862px-React-icon.svg.png" alt="React logo" width="200px"/>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(
+    navBar,
+    document.getElementById('root')
+);
+
+// ReactDOM.render(
+//     <div className="text">Some text practise</div>, 
+//     document.getElementById('root2')
+// );
